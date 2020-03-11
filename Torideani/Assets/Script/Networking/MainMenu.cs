df7cd87585cf39fcf778a1e3e2da3bc2bc2a3f65
Yassine Damiri <yassine.damiri@epita.fr>
2020-03-11 15:32:01 +0100

    Version Safe

--- Torideani/Assets/Script/Networking/MainMenu.cs
+++ Torideani/Assets/Script/Networking/MainMenu.cs
@@ -16,7 +16,7 @@ public class MainMenu : MonoBehaviourPunCallbacks
    private bool isConnecting = false;
 
    private const string GameVersion = "0.1";
-   private const int MaxPlayerPerRoom = 2;
+   private const int MaxPlayerPerRoom = 3;
 
    private void Awake()
    {
